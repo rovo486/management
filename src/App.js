a577695616cec49cca8bdb5ae11bfd569f0155db
rovo486 <rovo486@gmail.com>
2020-05-21 14:41:27 +0900

    create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
+
+const customers = [
+  {
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/1',
+    'name': '홍길동',
+    'birthday': '961222',
+    'gender': '남자',
+    'job': '대학생'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '홍길동2',
+    'birthday': '961222',
+    'gender': '남자2',
+    'job': '대학생2'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': '홍길동3',
+    'birthday': '961222',
+    'gender': '남자3',
+    'job': '대학생3'
+  }
+]
 
 class App extends Component {
   render(){
     return (
       <div className="gray-background">
-        <img src={logo} lat="logo" />
-        <h2>let's delvelop management system</h2>          
+        { 
+          customers.map( c => {
+            return(
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            )
+          })
+        } 
       </div>
     );
   }
